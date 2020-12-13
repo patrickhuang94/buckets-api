2cdb4580fb706409732614f636424430b042721b
Patrick Huang <patrickhuang94@gmail.com>
2020-12-12 18:56:43 -0800

    underscored doesn't work for timestamps :(

--- config/database.js
+++ config/database.js
@@ -3,5 +3,5 @@ const { Sequelize } = require('sequelize')
 module.exports = new Sequelize('buckets_development', 'root', '', {
   host: 'localhost',
   dialect: 'mysql',
-  define: { freezeTableName: true }, // prevent pluralizing table names
+  define: { freezeTableName: true, underscored: true }, // prevent pluralizing table names
 })
