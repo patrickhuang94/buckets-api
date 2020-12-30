2e6749163c1c466e3283522958f28747691e8937
Patrick Huang <patrickhuang94@gmail.com>
2020-12-29 22:16:59 -0800

    Let's try this

--- src/config/config.js
+++ src/config/config.js
@@ -9,7 +9,10 @@ module.exports = {
     dialect: 'mysql',
   },
   production: {
-    use_env_variable: process.env.DATABASE_URL,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.DATABASE_HOST,
     dialect: 'mysql',
   },
 }
