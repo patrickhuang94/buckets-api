2e635d6e4166bfc0808df2f008a3df3f45d31bba
Patrick Huang <patrickhuang94@gmail.com>
2020-12-29 22:04:34 -0800

    Configure database settings to read from .env

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,13 @@
 const { Sequelize } = require('sequelize')
 
-module.exports = new Sequelize('buckets_development', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
-  define: { freezeTableName: true, underscored: true }, // prevent pluralizing table names
-  logging: false,
-})
+module.exports = new Sequelize(
+  process.env.DATABASE,
+  process.env.DATABASE_USERNAME,
+  process.env.DATABASE_PASSWORD,
+  {
+    host: process.env.DATABASE_HOST,
+    dialect: 'mysql',
+    define: { freezeTableName: true, underscored: true }, // prevent pluralizing table names
+    logging: false,
+  }
+)
