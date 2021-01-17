4edd33a0ac669db1076b44d78003ae8696b37024
Patrick Huang <patrickhuang94@gmail.com>
2021-01-16 20:47:46 -0800

    Set up path config for dotenv

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
-require('dotenv').config()
+const path = require('path')
 const { Sequelize } = require('sequelize')
+require('dotenv').config({ path: path.join(__dirname, '../.env') })
 
 module.exports = new Sequelize(
   process.env.DATABASE,
