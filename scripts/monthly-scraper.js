3bb985737c172d151c97d07fc5e4790318e01472
Patrick Huang <patrickhuang94@gmail.com>
2021-01-23 14:59:34 -0800

    Only run job at beginning of month

--- scripts/monthly-scraper.js
+++ scripts/monthly-scraper.js
@@ -2,8 +2,23 @@ const ScheduleController = require('../app/controllers/schedule')
 
 const fetchSchedule = require('./fetch-schedule')
 
-async function schedules() {
-  const teamSchedules = await fetchSchedule({ month: 'january' })
+const months = {
+  0: 'january',
+  1: 'february',
+  2: 'march',
+  3: 'april',
+  4: 'may',
+  5: 'june',
+  6: 'july',
+  7: 'august',
+  8: 'september',
+  9: 'october',
+  10: 'november',
+  11: 'december',
+}
+
+async function schedules({ month }) {
+  const teamSchedules = await fetchSchedule({ month })
 
   for (const schedule of teamSchedules) {
     await ScheduleController.create({
@@ -16,11 +31,19 @@ async function schedules() {
 }
 
 async function main() {
-  console.log('Starting to scrape...')
+  const date = new Date()
+  const currentDate = date.getDate()
+  const currentMonth = date.getMonth()
 
-  await schedules()
+  // Heroku Scheduler doesn't support monthly jobs. Only run
+  // this on the first day of each month.
+  if (currentDate === 1) {
+    console.log('Starting to scrape...')
 
-  console.log('Done!')
+    await schedules({ month: months[currentMonth] })
+
+    console.log('Done!')
+  }
 }
 
 main()
